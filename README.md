9686a706dfdea96b911bbb3b8c590b645371ec96
Tony Williams <honestpuck@gmail.com>
2021-03-29 18:35:55 +1100

    list and out done

--- README.md
+++ README.md
@@ -1,16 +1,23 @@
 # scriptorium
-A utility for managing the scripts in Jamf Pro. It uses a git repo to back them up and as a source of truth.
+
+A utility for managing the scripts in Jamf Pro. It uses two git repos to back them up and as a source of truth.
 
 When it pulls a script from the server it attaches '&' and it's ID to the end of the name. It then stores the XML in one folder and extracts the script, storing it in a separate folder.
 
-`scriptorium` with no args lists all the scripts currently in the system.
+The capability set out below is currently a wish list rather than a totally implemented system.
+
+## Commands
+
+`scriptorium lists` lists all the scripts currently in the JPC.
+
+`scriptorium verify` compares the local scripts with the JPC and reports differences.
 
 `scriptorium out` pulls all scripts out of the server. It won't overwrite a script in storage unless the --force option is specified.
 
-`scriptorium in` does a git commit on the scripts folder. The pre-commit hook finds changed scripts in the scripts directory and copies them into the XML file and uploads the result to the JPC before doing a commit on the XML folder. 
+`scriptorium in` does a git commit on the scripts folder. The pre-commit hook finds changed scripts in the scripts directory and copies them into the XML file and uploads the result to the JPC.  It then does a commit on the XML folder. You can specify `--push` to do an immediate push after the commit. `--message 'string'` sets the commit message to `string`. `--no-commit` stops the commit
 
 The `in` option allows you to edit a script in the scripts directory and have it automatically backed up to the git repo and uploaded to the JPC.
-
+ 
 ### Changing a file name
 
 If you want to change a file name, perhaps to increment a version number, then it has to be done with the `scriptorium change` command. This takes the old name and new name as arguments. This command does a `git mv` on both the script and the XML file. It does _not_ do a commit.
@@ -19,14 +26,13 @@ If you want to change a file name, perhaps to increment a version number, then i
 
 Adding a script is done with `scriptorium add`. This expects a number of things to be defined on the command line. If they aren't defined on the command line the program will ask for them.
 
--F filename
--C category
--N Notes
--PR priority
--P0 No script parameters
--P4 parameter 4
--P11 parameter 11
+- -f filename
+- -c category
+- -n notes
+- -a priority=after
+- -b priority=before
+- -z zero parameters for script
 
-If any script parameters are defined on the command line, including P0, then the program will not ask for any. When it _does_ ask then the first blank parameter specified will stop it asking. Jamf supports up to number 11 so we do too.
+If `--zero` is specified then the program will not ask for any script parameters. When it _does_ ask the first blank parameter specified will stop it asking. Jamf supports from 4 to 11 parameters so we do too.
 
 It will then write out an XML file containing the info and an empty script.
--- scriptorium.py
+++ scriptorium.py
@@ -19,10 +19,10 @@ import requests
 LOGLEVEL = logging.DEBUG
  
 # where to stash the XML files
-xml_dir = "~/store/scriptorium/xml"
+xml_dir = "~/work/test/xml"
 
 # where to stash the script files
-sh_dir = "~/store/scriptorium/shell"
+sh_dir = "~/work/test/scripts"
 
 __all__ = [__name__]
 
@@ -38,22 +38,14 @@ class Jamf():
         self.cookies = ""
 
 
-class Parser():
-    """ our command line argument parser """
-
-    def __init__(self):
-        self.parser = argparse.ArgumentParser()
-
-
 class Scripts():
 
     def setup_logging():
         """Defines a nicely formatted logger"""
 
-        print("logging")
-        LOGFILE = "/usr/local/var/log/%s.log" % APPNAME
+        LOGFILE = "/usr/local/var/log/%s.log" % __name__
 
-        logger = logging.getLogger(APPNAME)
+        logger = logging.getLogger(__name__)
         ch = logging.handlers.TimedRotatingFileHandler(
             LOGFILE, when="D", interval=1, backupCount=7
         )
@@ -66,15 +58,142 @@ class Scripts():
         logger.addHandler(ch)
         logger.setLevel(LOGLEVEL)
 
+    def do_list(args, jpc):
+        """ subcommand `list` """
+        ret = requests.get(jpc.scriptsURL, auth=jpc.auth, headers=jpc.hdrs)
+        if ret.status_code != 200:
+            print("script list get failed with http error: %s" % 
+                ret.status_code)
+            exit(1)
+        for script in ret.json()['scripts']:
+            idn = script['id']
+            name = script['name']
+            print(f"{idn}:\t{name}")
+        exit()
+
+    def do_out(args, jpc):
+        """ subcommand `out` """
+        ret = requests.get(jpc.scriptsURL, auth=jpc.auth, headers=jpc.hdrs)
+        if ret.status_code != 200:
+            print("script list get failed with http error: %s" % 
+                ret.status_code)
+            exit(1)
+        for script in ret.json()['scripts']:
+            idn = script['id']
+            name = script['name']
+            # we want XML so don't set a header
+            ret = requests.get(f"{jpc.scriptsURL}/id/{idn}", auth=jpc.auth)
+            if ret.status_code != 200:
+                print(f"script get failed: {ret.status_code} : {ret.url}")
+                exit(1)
+            xml = ret.text
+            root = ET.fromstring(xml)
+            text = root.findtext('script_contents')
+            xml_filepath = f"{jpc.xml_out}/{name}"
+            sh_filepath = f"{jpc.sh_out}/{name}"
+            if args.force or not path.isfile(xml_filepath):
+                print(f"Writing XML {name}")
+                with open(xml_filepath, "w") as fp:
+                    fp.write(xml)
+            else:
+                print(f"XML {name} exists")
+            if args.force or not path.isfile(sh_filepath):
+                print(f"Writing script {name}")
+                with open(sh_filepath, "w") as fp:
+                    fp.write(text)
+            else:
+                print(f"Script {name} exists")
+        exit()
+
+    def do_in(args, jpc):
+        print("in not implemented")
+        exit()
+
+    def do_change(args, jpc):
+        print("change not implemented")
+        exit()
+
+    def do_rm(args, jpc):
+        print("rm not implemented")
+        exit()
+
+    def do_add(args, jpc):
+        print("add not implemented")
+        if args.filename:
+            print(args.filename)
+        exit()
+
+    def command_parser():
+        "build our command line parser"
+        parser = argparse.ArgumentParser(
+            epilog="for command help: `scriptorium <command> -h`")
+        subparsers = parser.add_subparsers(description="", required=True)
+
+        parser_ls = subparsers.add_parser('list',
+                        help="lists all scripts on the server")
+        parser_ls.set_defaults(func=Scripts.do_list)
+
+        # create parser for `out`
+        parser_out = subparsers.add_parser('out', 
+                        help="pulls all scripts out of the server")
+        parser_out.add_argument("-f", "--force", 
+                        help="force overwrite of existing script",
+                        action="store_true")
+        parser_out.set_defaults(func=Scripts.do_out)
+        
+        # create parser for 'in'
+        parser_in = subparsers.add_parser('in', 
+                        help="add new or changed scripts and commit")
+        parser_in.add_argument("-p", "--push", 
+                        help="do a git push after commit",
+                        action="store_true")
+        parser_in.set_defaults(func=Scripts.do_in)
+
+        # create parser for `change`
+        parser_ch = subparsers.add_parser('change',
+                        help="change the name of a script")
+        parser_ch.add_argument("name", help="current name of script")
+        parser_ch.add_argument("new", help="new name of script")
+        parser_ch.set_defaults(func=Scripts.do_change)
+
+        # create parser for `rm`
+        parser_rm = subparsers.add_parser('remove', help="remove (or delete) script from system")
+        parser_rm.add_argument("name", help="name of script to remove")
+        parser_rm.set_defaults(func=Scripts.do_rm)
+
+        # create parser for `add`
+        parser_add = subparsers.add_parser('add', help="add script to system")
+        parser_add.add_argument("-f", "--filename", help="name of new script")
+        parser_add.add_argument("-c", "--category", help="category of script")
+        parser_add.add_argument("-n", "--notes")
+        group = parser_add.add_mutually_exclusive_group()
+        group.add_argument("-a", "--after", 
+            help="run script with priority 'after'", action="store_true")
+        group.add_argument("-b", "--before", 
+            help="run script with priority 'before'", action="store_true")
+        group.add_argument("-r", "--reboot", 
+        help="run script at reboot", action="store_true")
+        parser_add.add_argument("-z", "--zero", help="zero parameters for script")
+        parser_add.set_defaults(func=Scripts.do_add)
+        
+        return parser
+
     def main(self):
-        self.setup_logging()
+        Scripts.setup_logging()
         logger.info("Start")
         jpc = Jamf()
-        timmy = Parser()
-        args = timmy.parse(sys.argv())
+        # sanity check
+        jpc.xml_out = path.expanduser(xml_dir)
+        jpc.sh_out = path.expanduser(sh_dir)
+        if not path.isdir(jpc.xml_out):
+            print ("directory {jpc.xml_out} does not exist")
+            exit(1)
+        if not path.isdir(jpc.sh_out):
+            print ("directory {jpc.sh_out} does not exist")
+            exit(1)
         # get prefs
         plist = path.expanduser(
-            "~/Library/Preferences/com.github.autopkg.plist"
+            "~/Library/Preferences/com.github.autopkg.stage.plist"
         )
         fp = open(plist, "rb")
         prefs = plistlib.load(fp)
@@ -84,18 +203,29 @@ class Scripts():
         # for the whole list JSON is handier so
         jpc.hdrs = {"Accept": "application/json"}
         jpc.auth = (prefs["API_USERNAME"], prefs["API_PASSWORD"])
+
         logger.debug("Prefs loaded")
 
-        # main routine
+        parser = Scripts.command_parser()
+        # handle no arguments on command line
+        if len(argv) == 1:
+            print("Missing subcommand")
+            parser.print_help()
+            exit()
+        args = parser.parse_args()
+        # we never return from below call
+        if args:
+            args.func(args, jpc)
+        else:
+            exit
+
         ret = requests.get(jpc.scriptsURL, auth=jpc.auth, headers=jpc.hdrs)
         if ret.status_code != 200:
             print("script list get failed: %s" % ret.status_code)
             exit()
-
-        # main loop
         for script in ret.json()['scripts']:
             idn = script['id']
-            print(f"{idn}: {script['name']}")
+            name = script['name']
             # we want XML so don't set a header
             ret = requests.get(f"{jpc.scriptsURL}/id/{idn}", auth=jpc.auth)
             if ret.status_code != 200:
